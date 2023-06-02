419a3fc3ce1006d1b260a695cf415ce2c305434d
Profesional Ingenieria II <soporte@pygmalion.com.co>
2023-06-02 16:27:13 -0500

    feat:Login style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,12 +7,12 @@ const Login = () => {
   const[password,setPassword] = useState("");
   const {createUser, loginUser} = UserAuth();
   const navigate = useNavigate();
+
   /* const handleSubmit = async (event) =>{ 
     event.preventDefault(); //evita que el formulario se recargue por defecto 
     console.log(email,password);
     await createUser(email,password);
-  }
- */
+  }*/
 
     const handleSubmit = async (event) =>{
       event.preventDefault();
@@ -21,30 +21,52 @@ const Login = () => {
     }
   return (
 
-    <div>
-      <form className='w-max h-max border border-zinc-950 rounded flex flex-col items-center' onSubmit={handleSubmit}>
-        <div className='p-1 w-full'>
-          <label >E-mail: </label>
-        <input className='w-full border border-zinc-950 rounded ' type = "email"  onChange={(event)=>setEmail(event.target.value)}/>
-        </div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-sky-950 to-sky-400 ">
+      <div className="px-10 py-10 mt-4 text-left shadow-2xl rounded-2xl">
+        <h3 className="text-4xl font-bold text-center p-5 ">Login</h3>
+        <form className='w-max 
+                        h-max  
+                        rounded-2xl 
+                        flex 
+                        flex-col 
+                        items-center
+                        bg-sky-800
+                        shadow-2xl' 
+                onSubmit={handleSubmit}>
 
-        <div className='p-1 w-full'>
-          <label>Password: </label>
-          <input className='w-full 
-                          border 
-                          border-zinc-950 
-                          rounded' 
-                type = "password" 
-                onChange={(event)=>setPassword(event.target.value)}
-          />
-        </div>
-        <div className='p-1'>
-          <button  className='border border-teal-400 rounded p-1 hover:bg-teal-400 hover:text-whit'>Enviar</button>
-        </div>
-        
-      </form>
+          <div className='p-3 w-full'>
+            <label className='text-base font-medium'>E-mail: </label>
+            <input className='w-full
+                              rounded-md' 
+                    placeholder="   Email"
+                    type = "Email"  
+                    onChange={(event)=>setEmail(event.target.value)}/>
+          </div>
+
+          <div className='p-3 w-full'>
+            <label className='text-base font-medium'>Password: </label>
+            <input className='w-full
+                            rounded-md'
+                  placeholder="   Password" 
+                  type = "Password" 
+                  onChange={(event)=>setPassword(event.target.value)}
+            />
+          </div>
+
+          <div className='p-2 pb-5'>
+            <button className='px-6 
+                              py-2 
+                              mt-4 
+                              text-white 
+                              bg-sky-950 
+                              rounded-lg 
+                              hover:bg-sky-100
+                              hover:text-sky-950'>Enviar</button>
+          </div>
+          
+        </form>
+      </div>
     </div>
-
   )
 }
 
