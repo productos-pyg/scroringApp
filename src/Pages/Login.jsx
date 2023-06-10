0e714e3094fba4377bdb9c70175812c5b9f1a573
Profesional Ingenieria II <soporte@pygmalion.com.co>
2023-06-10 08:10:12 -0500

    feat:Style-login-v2

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -22,37 +22,39 @@ const Login = () => {
     }
   return (
 
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-[#0c0c0c] to-[#b2b2b2] ">
-      <div>
-        <img src={LogoWhite} className='h-10 pl-4 '/>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-[#063653] to-[#1097d5] ">
+      <div className='flex center items-center justify-center '>
+        <img className='w-2/5' src={LogoWhite} />
       </div>
       
       <div className="px-10 py-10 mt-4 text-left shadow-2xl rounded-2xl">
-        <h3 className="text-4xl font-bold text-center p-5 ">Login</h3>
+        <h3 className="text-4xl font-corbel text-center p-5  text-[#ffffff]">Login</h3>
         <form className='w-max 
                         h-max  
                         rounded-2xl 
                         flex 
                         flex-col 
                         items-center
-                        bg-[#626567]
+                        bg-[#1097d557]
                         shadow-2xl' 
                 onSubmit={handleSubmit}>
 
           <div className='p-3 w-full'>
-            <label className='text-base font-medium'>E-mail: </label>
+            <label className='text-base font-corbel text-[#ffffff]'>E-mail: </label>
             <input className='w-full
+                              pl-3
                               rounded-md' 
-                    placeholder="   Email"
+                    placeholder="Email"
                     type = "Email"  
                     onChange={(event)=>setEmail(event.target.value)}/>
           </div>
 
           <div className='p-3 w-full'>
-            <label className='text-base font-medium'>Password: </label>
+            <label className='text-base font-corbel text-[#ffffff]'>Password: </label>
             <input className='w-full
-                            rounded-md'
-                  placeholder="   Password" 
+                              pl-3
+                              rounded-md'
+                  placeholder="Password" 
                   type = "Password" 
                   onChange={(event)=>setPassword(event.target.value)}
             />
@@ -61,12 +63,13 @@ const Login = () => {
           <div className='p-2 pb-5'>
             <button className='px-6 
                               py-2 
-                              mt-4 
+                              mt-4
+                              font-corbel
                               text-white 
-                              bg-[#0c0c0c]
+                              bg-[#1097d5]
                               rounded-lg 
                               hover:bg-sky-100
-                              hover:text-[#0c0c0c]'>Enviar</button>
+                              hover:text-[#1097d5]'>Enviar</button>
           </div>
           
         </form>
