342c01c1111cbe059b2ca15f6e51f728871ed97b
Profesional Ingenieria II <soporte@pygmalion.com.co>
2023-07-06 14:56:06 -0500

    feat:Style form newTeams

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import { UserAuth } from "../context/AuthContext"
 import Wrap from "../components/Wrap"
+import Edi from "../assets/img/SRI-Edi.png"
 
 const Home = () => {
   //const{user} = UserAuth();
@@ -7,7 +8,9 @@ const Home = () => {
   //console.log(user)
   return (
     <Wrap>
-          <div>home</div>
+      <div className='w-1/2 h-3/5 flex items-center ml-20 mt-20'>
+        <img src={Edi} className='w-3/5'/>
+      </div>
     </Wrap>
   )
 }
--- src/Pages/NewTeams.jsx
+++ src/Pages/NewTeams.jsx
@@ -72,9 +72,8 @@ const NewTeams = () => {
 
   return (
     <Wrap>
-      <button>        
+      <button>      
         <Link className='flex
-                        mb-5
                         flex-row 
                         items-center 
                         border 
@@ -85,7 +84,10 @@ const NewTeams = () => {
                         text-white 
                         bg-[#1097d5]
                         hover:bg-[#ffffff]
-                        hover:text-[#1097d5]' to="/teams">
+                        hover:text-[#1097d5]
+                        shadow-lg 
+                        shadow-[#1097d5]/100' 
+              to="/events">
           <TiArrowBackOutline className=''/>
           <span className='mx-2'>Atras</span>
         </Link>
@@ -93,97 +95,132 @@ const NewTeams = () => {
 
       <div className='h-screen
                       w-screen'>
-        <form className= 'w-10/12 flex flex-col' 
+        <form className= 'w-10/12 flex flex-col mt-5' 
         
         onSubmit={handleSubmit(onSubmit)}>
             <label className='mt-2'>Nombre del Equipo</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' placeholder='name' required onChange={e => setTeamsName(e.target.value)} {...register("teamsName")}></input>
+            <input className='pl-2 py-2 border rounded-lg' type='text' placeholder='Name' required onChange={e => setTeamsName(e.target.value)} {...register("teamsName")}></input>
 
-            <label className='mt-2'>Entidad a la que pertenece</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' onChange={e => setTeamsEntity(e.target.value)} {...register("teamsEntity")}></input>
+            <div className="flex flex-row items-center justify-between">
+              <div className="flex flex-col w-2/3 mr-5">
+                <label className='mt-2'>Entidad a la que pertenece</label>
+                <input className='pl-2 py-2 border rounded-lg' type='text' onChange={e => setTeamsEntity(e.target.value)} {...register("teamsEntity")}></input>
+              </div>
+              <div className="flex flex-col w-1/3">
+                <label className='mt-2'>Ciudad</label>
+                <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setTeamsCity(e.target.value)} {...register("teamsCity")}></input>
+              </div>
+            </div>
 
-            <label className='mt-2'>Ciudad</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setTeamsCity(e.target.value)} {...register("teamsCity")}></input>
-            
-            <label className='mt-2'>Evento</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setEventsType(e.target.value)} {...register("EventsType")}></input>
+            <div className="flex flex-row items-center justify-between mb-10">
+              <div className="flex flex-col w-2/5 mr-5">
+                <label className='mt-2'>Evento</label>
+                <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setEventsType(e.target.value)} {...register("EventsType")}></input>
+              </div>
+              <div className="flex flex-col w-1/2 mr-5">
+                <label className='mt-2'>Reto</label>
+                <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setChallengeType(e.target.value)} {...register("challengesType")}></input>
+              </div>
+              <div className="flex flex-col w-1/5">
+                <label className='mt-2'>Numero de participantes</label>
+                <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setTeamsNumbMembers(e.target.value)} {...register("TeamsNumbMembers")}></input>
+              </div>
+            </div>
 
-            <label className='mt-2'>Reto</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setChallengeType(e.target.value)} {...register("challengesType")}></input>
+            <fieldset className='border border-dashed border-gray-400 px-5 pt-2 pb-3 mb-10'>
+            <legend className='text-gray-400 font-bold'>Datos del Entrenador</legend>
+              <div className="flex flex-col mb-5">
+                  <label className='mt-2'>Nombre completo</label>
+                  <input className='pl-2 py-2 border rounded-lg' type='text' placeholder='Full name' required onChange={e => setCoachName(e.target.value)} {...register("coachName")}></input>
+                
+                <div className="flex flex-row items-center justify-between">
+                  <div className="flex flex-col w-1/3 mr-5">
+                    <label className='mt-2'>Numero de contacto</label>
+                    <input className='pl-2 py-2 border rounded-lg' type='tel' required onChange={e => setCoachPhone(e.target.value)} {...register("coachPhone")}></input>
+                  </div>
+                  <div className="flex flex-col w-2/3">
+                    <label className='mt-2'>Correo electronico</label>
+                    <input className='pl-2 py-2 border rounded-lg' type='email' required onChange={e => setCoachEmail(e.target.value)} {...register("coachEmail")}></input>
+                  </div>
+                </div>
 
-            <label className='mt-2'>Numero de participantes</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' required onChange={e => setTeamsNumbMembers(e.target.value)} {...register("TeamsNumbMembers")}></input>
-
-            <fieldset>
-            <legend>Datos del Entrenador</legend>
-            <label className='mt-2'>Nombre completo</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' placeholder='name' required onChange={e => setCoachName(e.target.value)} {...register("coachName")}></input>
-
-            <label className='mt-2'>Fecha de nacimiento</label>
-            <input className='pl-2 py-2 border rounded-lg' type='date' required onChange={e => setCoachDate(e.target.value)} {...register("coachDate")}></input>
-
-            <label className='mt-2'>Numero de identificación</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text'  required onChange={e => setCoachId(e.target.value)} {...register("coachId")}></input>
-            
-            <label className='mt-2'>Numero de contacto</label>
-            <input className='pl-2 py-2 border rounded-lg' type='tel' required onChange={e => setCoachPhone(e.target.value)} {...register("coachPhone")}></input>
-            
-            <label className='mt-2'>Correo electronico</label>
-            <input className='pl-2 py-2 border rounded-lg' type='email' required onChange={e => setCoachEmail(e.target.value)} {...register("coachEmail")}></input>
-
-            <label className='mt-2'>Talla de camiseta</label>
-            <select className='pl-2 py-2 border rounded-lg' required onChange={e => setCoachSize(e.target.value)} {...register("coachSize")}>
-              <option ></option>
-              <option >XS</option>
-              <option >S</option>
-              <option >M</option>
-              <option >L</option>
-              <option >XL</option>
-              <option >XXL</option>
-            </select>
+                <div className="flex flex-row items-center justify-between">
+                  <div className="flex flex-col w-1/3 mr-5">
+                    <label className='mt-2'>Fecha de nacimiento</label>
+                    <input className='pl-2 py-2 border rounded-lg' type='date' required onChange={e => setCoachDate(e.target.value)} {...register("coachDate")}></input>
+                  </div>
+                  <div className="flex flex-col w-1/3 mr-5">
+                    <label className='mt-2'>Numero de identificación</label>
+                    <input className='pl-2 py-2 border rounded-lg' type='text'  required onChange={e => setCoachId(e.target.value)} {...register("coachId")}></input>
+                  </div>
+                  <div className="flex flex-col w-1/3">
+                    <label className='mt-2'>Talla de camiseta</label>
+                    <select className='pl-2 py-2 border rounded-lg' required onChange={e => setCoachSize(e.target.value)} {...register("coachSize")}>
+                      <option ></option>
+                      <option >XS</option>
+                      <option >S</option>
+                      <option >M</option>
+                      <option >L</option>
+                      <option >XL</option>
+                      <option >XXL</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
             </fieldset>
 
-            <fieldset>
-            <legend>Datos de los Participantes</legend>
-            <label className='mt-2'>Nombre completo</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text' placeholder='name' required onChange={e => setMemberName(e.target.value)} {...register("memberName")}></input>
+            <fieldset className='border border-dashed border-gray-400 px-5 pt-2'>
+            <legend className='text-gray-400 font-bold'>Datos de los Participantes</legend>
+              <div className="flex flex-col mb-8">
+                  <label className='mt-2'>Nombre completo</label>
+                  <input className='pl-2 py-2 border rounded-lg' type='text' placeholder='Full name' required onChange={e => setCoachName(e.target.value)} {...register("coachName")}></input>
 
-            <label className='mt-2'>Fecha de nacimiento</label>
-            <input className='pl-2 py-2 border rounded-lg' type='date' required onChange={e => setMemberDate(e.target.value)} {...register("memberDate")}></input>
-
-            <label className='mt-2'>Numero de identificación</label>
-            <input className='pl-2 py-2 border rounded-lg' type='text'  required onChange={e => setMemberId(e.target.value)} {...register("memberId")}></input>
-            
-            <label className='mt-2'>Talla de camiseta</label>
-            <select className='pl-2 py-2 border rounded-lg' required onChange={e => setMemberSize(e.target.value)} {...register("memberSize")}>
-              <option ></option>
-              <option >XS</option>
-              <option >S</option>
-              <option >M</option>
-              <option >L</option>
-              <option >XL</option>
-              <option >XXL</option>
-            </select>
+                <div className="flex flex-row items-center justify-between">
+                  <div className="flex flex-col w-1/3 mr-5">
+                    <label className='mt-2'>Fecha de nacimiento</label>
+                    <input className='pl-2 py-2 border rounded-lg' type='date' required onChange={e => setCoachDate(e.target.value)} {...register("coachDate")}></input>
+                  </div>
+                  <div className="flex flex-col w-1/3 mr-5">
+                    <label className='mt-2'>Numero de identificación</label>
+                    <input className='pl-2 py-2 border rounded-lg' type='text'  required onChange={e => setCoachId(e.target.value)} {...register("coachId")}></input>
+                  </div>
+                  <div className="flex flex-col w-1/3">
+                    <label className='mt-2'>Talla de camiseta</label>
+                    <select className='pl-2 py-2 border rounded-lg' required onChange={e => setCoachSize(e.target.value)} {...register("coachSize")}>
+                      <option ></option>
+                      <option >XS</option>
+                      <option >S</option>
+                      <option >M</option>
+                      <option >L</option>
+                      <option >XL</option>
+                      <option >XXL</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
             </fieldset>
-
-            <button className='flex
-
-                              w-1/12
-                              mb-5
-                              flex-row 
-                              items-center 
-                              border 
-                              rounded-md 
-                              p-2 
-                              mt-4
-                              font-corbel
-                              text-white 
-                              bg-[#1097d5]
-                              hover:bg-[#ffffff]
-                              hover:text-[#1097d5]'>
-            <RiMailSendLine/>
-            <span class="ml-2">Enviar</span>
-            </button>
+            <div className='w-full flex items-center justify-center'>
+              <button className='flex
+                                w-32
+                                mb-5
+                                flex-row 
+                                items-center
+                                justify-center
+                                border 
+                                rounded-md 
+                                p-2 
+                                mt-4
+                                font-corbel
+                                text-white 
+                                bg-[#1097d5]
+                                hover:bg-[#ffffff]
+                                hover:text-[#1097d5]
+                                shadow-lg 
+                              shadow-[#1097d5]/100'>
+              <RiMailSendLine/>
+              <span className="ml-2">Enviar</span>
+              </button>
+            </div>
         </form>
       </div>
     </Wrap>
